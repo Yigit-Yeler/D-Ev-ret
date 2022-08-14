d17ace1af349e6edcf07b653ddc548fd41125724
YigitYeler <yeleryigit40@gmail.com>
2022-08-14 18:41:23 +0300

    Screens :: added signup screen logic

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,7 +1,14 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { signUp } from '../styles/signUpStyles'
 const SignUp = () => {
+
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [rePassword, setRePassword] = useState('')
+
     return (
         <View style={signUp.main}>
             <View style={signUp.logo}>
@@ -9,11 +16,31 @@ const SignUp = () => {
             </View>
 
             <View style={signUp.textInputView}>
-                <TextInput placeholder='Email' style={signUp.textInput} />
-                <TextInput placeholder='Email' style={signUp.textInput} />
-                <TextInput placeholder='Email' style={signUp.textInput} />
-                <TextInput placeholder='Email' style={signUp.textInput} />
-                <TextInput placeholder='Email' style={signUp.textInput} />
+                <TextInput
+                    placeholder='Name'
+                    style={signUp.textInput}
+                    onChangeText={(text) => { setName(text) }}
+                />
+                <TextInput
+                    placeholder='Surname'
+                    style={signUp.textInput}
+                    onChangeText={(text) => { setSurname(text) }}
+                />
+                <TextInput
+                    placeholder='E-mail'
+                    style={signUp.textInput}
+                    onChangeText={(text) => { setEmail(text) }}
+                />
+                <TextInput
+                    placeholder='Password'
+                    style={signUp.textInput}
+                    onChangeText={(text) => { setPassword(text) }}
+                />
+                <TextInput
+                    placeholder='Password Again'
+                    style={signUp.textInput}
+                    onChangeText={(text) => { setRePassword(text) }}
+                />
             </View>
 
             <View style={signUp.signUpView}>
