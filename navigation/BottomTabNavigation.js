423f7b5036fa7d0c326c8203f830514b61bc8a71
YigitYeler <yeleryigit40@gmail.com>
2022-08-19 13:41:46 +0300

    Navigation :: added bottom tab navigator

---
+++ navigation/BottomTabNavigation.js
@@ -0,0 +1,67 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// const myIcon = <Icon name="rocket" size={30} color="#900" />;
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import MyDm from '../src/screens/MyDm';
+import CreatePost from '../src/screens/CreatePost';
+import Home from '../src/screens/Home';
+import Profile from '../src/screens/Profile';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigation = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            activeColor="white"
+            barStyle={{ backgroundColor: 'black' }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="CreatePost"
+                component={CreatePost}
+                options={{
+                    tabBarLabel: 'Create Post',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="MyDm"
+                component={MyDm}
+                options={{
+                    tabBarLabel: 'Messages',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="message-text" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTabNavigation
+
+
--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -3,14 +3,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from '../src/screens/Home';
 import Chat from '../src/screens/Chat';
 import SignIn from '../src/screens/SignIn';
 import SignUp from '../src/screens/SignUp';
-import Profile from '../src/screens/Profile';
 import MyPosts from '../src/screens/MyPosts';
-import MyDm from '../src/screens/MyDm';
-import CreatePost from '../src/screens/CreatePost';
+
+import BottomTabNavigation from './BottomTabNavigation';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,13 +16,10 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="BottomTab" component={BottomTabNavigation} />
                 <Stack.Screen name="SignUp" component={SignUp} />
                 <Stack.Screen name="SignIn" component={SignIn} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Profile" component={Profile} />
-                <Stack.Screen name="CreatePost" component={CreatePost} />
                 <Stack.Screen name="MyPosts" component={MyPosts} />
-                <Stack.Screen name="DM" component={MyDm} />
                 <Stack.Screen name="Chat" component={Chat} />
             </Stack.Navigator>
         </NavigationContainer>
