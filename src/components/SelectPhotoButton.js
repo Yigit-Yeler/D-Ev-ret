da014a70daf5e932a9f510a1ebb682088a1e2e07
YigitYeler <yeleryigit40@gmail.com>
2022-09-16 17:06:10 +0300

    Component :: showed selected images on button

--- src/components/SelectPhotoButton.js
+++ src/components/SelectPhotoButton.js
@@ -2,49 +2,26 @@ import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native'
 import React from 'react'
 import { selectPhotoButtonStyles } from './styles/selectPhotoButtonStyles'
 const SelectPhotoButton = ({ photos, onPress }) => {
-
-    const listData = [
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-        // {
-        //     'img': 'https://static.wikia.nocookie.net/hunterxhunter/images/b/bd/HxH2011_EP147_Killua_Portrait.png/revision/latest?cb=20220624211000'
-        // },
-    ]
-
     return (
         <View>
             {
-                listData[0] ? (
-                    <View style={selectPhotoButtonStyles.selectPhotoButton}>
+                photos ? (
+                    <TouchableOpacity
+                        onPress={onPress()}
+                        style={selectPhotoButtonStyles.selectPhotoButton}>
                         <FlatList
-                            data={listData}
+                            data={photos.selected}
                             renderItem={({ item, index }) => {
-                                if (listData.length > 4 && index == 3) {
+                                if (photos.selected.length > 4 && index == 3) {
                                     return (
                                         <View>
-                                            <Image style={selectPhotoButtonStyles.img} source={{ uri: item.img }} />
+                                            <Image style={selectPhotoButtonStyles.img} source={{ uri: item.uri }} />
                                             <View style={selectPhotoButtonStyles.otherImgCount} >
 
                                             </View>
                                             <View style={selectPhotoButtonStyles.otherImgCountText} >
                                                 <Text style={selectPhotoButtonStyles.numText}>
-                                                    +{listData.length - 4}
+                                                    +{photos.selected.length - 4}
                                                 </Text>
                                             </View>
                                         </View>
@@ -52,7 +29,7 @@ const SelectPhotoButton = ({ photos, onPress }) => {
                                 }
                                 else if (index < 4) {
                                     return (
-                                        <Image style={selectPhotoButtonStyles.img} source={{ uri: item.img }} />
+                                        <Image style={selectPhotoButtonStyles.img} source={{ uri: item.uri }} />
                                     )
                                 }
 
@@ -60,7 +37,7 @@ const SelectPhotoButton = ({ photos, onPress }) => {
                             numColumns={2}
 
                         />
-                    </View>
+                    </TouchableOpacity>
 
                 ) : (
                     <TouchableOpacity
